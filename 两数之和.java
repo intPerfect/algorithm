40226c8a57776d22bb105e0ba2c936b8fc712819
intPerfect <19511981689@163.com>
2025-04-04 21:58:30 +0800

    feat: 添加两数之和解法和.gitignore配置

---
+++ 两数之和.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 将 x + y = target 转化为 x = target - y
+            int key = target - nums[i];
+            // 查表得到 x 的索引
+            if (hash.containsKey(key)) {
+                return new int[] { hash.get(key), i };
+            } else {
+                // 添加当前元素的索引
+                hash.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
+
+public class 两数之和 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // 测试用例1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        System.out.println(Arrays.toString(solution.twoSum(nums1, target1))); // 输出: [0, 1]
+
+        // 测试用例2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        System.out.println(Arrays.toString(solution.twoSum(nums2, target2))); // 输出: [1, 2]
+
+        // 测试用例3
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        System.out.println(Arrays.toString(solution.twoSum(nums3, target3))); // 输出: [0, 1]
+    }
+}
